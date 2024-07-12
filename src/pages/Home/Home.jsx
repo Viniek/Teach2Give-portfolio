acb8415d2805c229ee83bcaeeee4f9b70372a33f
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-07-12 18:53:45 +0300

    reset css and add content to home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,17 +6,20 @@ import React from 'react'
 function Home() {
   return (
     <>
+    <div className="johninfo">
     <div className="aboutjohn">
       <h1 className="heading">Hi, I am John, <br />Creative Technologist</h1>
 
-      <p>Amet minim mollit non deserunt ulliamco est sit aliqua dolor do amet slint. Velit molit. Exercitation veniam
-        consequat sunt nonstrud amet
+      <p>Amet minim mollit non deserunt ulliamco est sit aliqua dolor do amet  slint. Velit molit. Exercitation veniam 
+      consequat sunt nonstrud amet
       </p>
 
       <button>Download Resume</button>
     </div>
 
-    <img src="https://res.cloudinary.com/dukptptve/image/upload/v1720798459/nost2u1thpsv62ewjisr.png" alt="" />
+    <img src="https://res.cloudinary.com/dukptptve/image/upload/v1720798459/nost2u1thpsv62ewjisr.png   " alt="john" />
+    </div>
+
     </>
   )
 }
