433f59147b8633b11f97d19208397d4f61182359
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-07-12 18:48:13 +0300

    add text and an image

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import "./Home.css"
 
 import React from 'react'
@@ -6,7 +6,17 @@ import React from 'react'
 function Home() {
   return (
     <>
-    
+    <div className="aboutjohn">
+      <h1 className="heading">Hi, I am John, <br />Creative Technologist</h1>
+
+      <p>Amet minim mollit non deserunt ulliamco est sit aliqua dolor do amet slint. Velit molit. Exercitation veniam
+        consequat sunt nonstrud amet
+      </p>
+
+      <button>Download Resume</button>
+    </div>
+
+    <img src="https://res.cloudinary.com/dukptptve/image/upload/v1720798459/nost2u1thpsv62ewjisr.png" alt="" />
     </>
   )
 }
