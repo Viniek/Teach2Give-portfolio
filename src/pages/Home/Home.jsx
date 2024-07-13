0789146d2d0b70bd5f8fd28ae0520d13cd8bb4af
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-07-13 11:31:39 +0300

    add designs

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -20,6 +20,30 @@ function Home() {
     <img src="https://res.cloudinary.com/dukptptve/image/upload/v1720798459/nost2u1thpsv62ewjisr.png   " alt="john"  className="john"/>
     </div>
 
+<section className="post">  
+<div className="titles">
+  <h2>Recent posts</h2>
+  <p>View all</p>
+</div>
+<div className="postsdesign">
+<div className="makingdesign">
+  <h2>Making a design system from scratch</h2>
+  <p className="designdate">12 Feb 2020   |   Design Pattern</p>
+
+  <p className="designcontent">Amet minium mollit non deserut uliaamco est sit <br /> aliqua dolor do amet sint. Velit officia consequat <br />  duis enim velit mollit.Exerciaation veniam <br />consequat sunt nonstrud amet.</p>
+</div>
+
+
+<div className="makingdesign">
+  <h2>Making a design system from scratch</h2>
+  <p className="designdate">12 Feb 2020   |   Design Pattern</p>
+
+  <p className="designcontent">Amet minium mollit non deserut uliaamco est sit <br /> aliqua dolor do amet sint. Velit officia consequat <br />  duis enim velit mollit.Exerciaation veniam <br />consequat sunt nonstrud amet.</p>
+</div>
+</div>
+</section>
+    
+
     </>
   )
 }
