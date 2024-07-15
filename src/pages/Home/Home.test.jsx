d049c77814dc90989476095a0595f2664f2c069d
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-07-15 16:00:17 +0300

    finish home testing

--- src/pages/Home/Home.test.jsx
+++ src/pages/Home/Home.test.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import '@testing-library/jest-dom';
 import { describe, it, expect } from 'vitest';
 import Home from './Home';
 
@@ -12,11 +11,29 @@ describe('Home', () => {
         <Home />
       </MemoryRouter>
     );
+
+
     const headingElement = screen.getByText(/Hi, I am John, Creative Technologist/i);
     expect(headingElement).toBeInTheDocument();
 
+ 
     const buttonElement = screen.getByText(/Download Resume/i);
     expect(buttonElement).toBeInTheDocument();
 
+
+    const imageElement = screen.getByAltText('john');
+    expect(imageElement).toBeInTheDocument();
+
+
+    const recentPostsHeading = screen.getByText(/Recent posts/i);
+    expect(recentPostsHeading).toBeInTheDocument();
+
+   
+    const firstPostHeading = screen.getByText(/Making a design system from scratch/i);
+    expect(firstPostHeading).toBeInTheDocument();
+
+   
+    const secondPostHeading = screen.getByText(/Creating pixel perfect icons in Figma/i);
+    expect(secondPostHeading).toBeInTheDocument();
   });
 });
