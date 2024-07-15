c3e55352f65ccaa8e91b7332ff36963c84bc9360
AllanMuhari <allanmuhari087@gmail.com>
2024-07-15 10:58:33 +0300

    fixed width

--- src/pages/Works/Works.jsx
+++ src/pages/Works/Works.jsx
@@ -1,76 +1,148 @@
-import React from 'react'
-import dashboard from '../../assets/dashboard.png'
-import  './Works.css'
+import React from "react";
+import dashboard from "../../assets/dashboard.png";
+import "./Works.css";
 
 function Works() {
   return (
-   <>
-   <section className='work'>
+    <>
+      <section className="work">
+        <h className="header1">Work</h>
 
+        <section className="work_container">
+          <img
+            src="https://res.cloudinary.com/dgn62le6w/image/upload/v1720860598/el7tftyhpqdtzh38czg0.png
+"
+            alt="Dashboard"
+          />
+          <div className="worktext">
+            <h2>
+              <b>Designing Dashboards</b>
+            </h2>
+            <p className="paragraph1">
+              <span>
+                <b>2020</b>
+              </span>{" "}
+              Dashboard
+              <br />
+            </p>
+            <p className="paragraph2">
+              Amet minim deserunt ullamco est sit aliqua dolor do amet
+              sint.Velit officia consequat duis enim velit mollit.Exercitation
+              veniam consequat sunt sunta .
+            </p>
+          </div>
+        </section>
 
-    <h className='header1'>Work</h>
-    
-    <section className='work_container'>      
-<img src="https://res.cloudinary.com/dgn62le6w/image/upload/v1720860598/el7tftyhpqdtzh38czg0.png
-" alt="Dashboard" />
-<div className='worktext'>
-<h2><b>Designing Dashboards</b></h2>
-<p className='paragraph1'><span><b>2020</b></span>  Dashboard<br/></p>
-<p className='paragraph2'>Amet minim deserunt ullamco est sit aliqua dolor do amet sint.Velit officia consequat duis enim velit mollit.Exercitation veniam consequat sunt sunta .</p>
-</div>
-    </section>
+        <section className="work_container_default">
+          <section className="work_container">
+            <img
+              src="https://res.cloudinary.com/dgn62le6w/image/upload/v1720873210/dwksimg2boi9pe5aq0ts.png
+"
+              alt="Dashboard"
+            />
+            <div className="worktext">
+              <h2>
+                <b>Vibrant Potraits of 2020</b>
+              </h2>
+              <p className="paragraph1">
+                <span>
+                  <b>2018</b>
+                </span>{" "}
+                Illustration
+                <br />
+              </p>
+              <p className="paragraph2">
+                Amet minim deserunt ullamco est sit aliqua dolor do amet
+                sint.Velit officia consequat duis enim velit mollit.Exercitation
+                veniam consequat sunt sunta .
+              </p>
+            </div>
+          </section>
 
-<section className='work_container_default'>
-    <section className='work_container'>      
-<img src="https://res.cloudinary.com/dgn62le6w/image/upload/v1720873210/dwksimg2boi9pe5aq0ts.png
-" alt="Dashboard" />
-<div className='worktext'>
-<h2><b>Vibrant Potraits of 2020</b></h2>
-<p className='paragraph1'><span><b>2018</b></span>  Illustration<br/></p>
-<p className='paragraph2'>Amet minim deserunt ullamco est sit aliqua dolor do amet sint.Velit officia consequat duis enim velit mollit.Exercitation veniam consequat sunt sunta .</p>
-</div>
-    </section>
+          <section className="work_container">
+            <img
+              src="https://res.cloudinary.com/dgn62le6w/image/upload/v1720873340/cbkstuslplyc8pldolt5.png
+"
+              alt="Dashboard"
+            />
 
-    <section className='work_container'>      
-<img src="https://res.cloudinary.com/dgn62le6w/image/upload/v1720873340/cbkstuslplyc8pldolt5.png
-" alt="Dashboard" />
+            <div className="worktext">
+              <h2>
+                <b>36 Days of Malayalam type</b>
+              </h2>
+              <p className="paragraph1">
+                <span>
+                  <b>2018</b>
+                </span>{" "}
+                Typography
+                <br />
+              </p>
+              <p className="paragraph2">
+                Amet minim deserunt ullamco est sit aliqua dolor do amet
+                sint.Velit officia consequat duis enim velit mollit.Exercitation
+                veniam consequat sunt sunta .
+              </p>
+            </div>
+          </section>
 
-<div className='worktext'>
-<h2><b>36 Days of Malayalam type</b></h2>
-<p className='paragraph1'><span><b>2018</b></span>  Typography<br/></p>
-<p className='paragraph2'>Amet minim deserunt ullamco est sit aliqua dolor do amet sint.Velit officia consequat duis enim velit mollit.Exercitation veniam consequat sunt sunta .</p>
-</div>
-    </section>
+          <section className="work_container">
+            <img
+              src="https://res.cloudinary.com/dgn62le6w/image/upload/v1720873408/lpmdfdiqwtpt0wzo7whm.png
+"
+              alt="Dashboard"
+            />
 
-    <section className='work_container'>      
-<img src="https://res.cloudinary.com/dgn62le6w/image/upload/v1720873408/lpmdfdiqwtpt0wzo7whm.png
-" alt="Dashboard" />
+            <div className="worktext">
+              <h2>
+                <b>Components</b>
+              </h2>
+              <p className="paragraph1">
+                <span>
+                  <b>2018</b>
+                </span>{" "}
+                Components
+                <br />
+              </p>
+              <p className="paragraph2">
+                Amet minim deserunt ullamco est sit aliqua dolor do amet
+                sint.Velit officia consequat duis enim velit mollit.Exercitation
+                veniam consequat sunt sunta .
+              </p>
+            </div>
+          </section>
+        </section>
 
-<div className='worktext'>
-<h2><b>Components</b></h2>
-<p className='paragraph1'><span><b>2018</b></span>  Components<br/></p>
-<p className='paragraph2'>Amet minim deserunt ullamco est sit aliqua dolor do amet sint.Velit officia consequat duis enim velit mollit.Exercitation veniam consequat sunt sunta .</p>
-</div>
-    </section>
+        {/* landing */}
+        <div className="landing">
+          <section className="work_container">
+            <img
+              src="https://res.cloudinary.com/dgn62le6w/image/upload/v1720882784/g09n0uq4t3n41mgoxnip.png
+"
+              alt="Dashboard"
+            />
 
-    </section>
-
-    {/* landing */}
-    <div className='landing'>
-    <section className='work_container'>      
-<img src="https://res.cloudinary.com/dgn62le6w/image/upload/v1720882784/g09n0uq4t3n41mgoxnip.png
-" alt="Dashboard" />
-
-<div className='worktext'>
-<h2><b>Designing Landing page</b></h2>
-<p className='paragraph1'><span><b>2018</b></span>  Website<br/></p>
-<p className='paragraph2'>Amet minim deserunt ullamco est sit aliqua dolor do amet sint.Velit officia consequat duis enim velit mollit.Exercitation veniam consequat sunt sunta .</p>
-</div>
-    </section>
-</div>
-   </section>
-   </>
-  )
+            <div className="worktext">
+              <h2>
+                <b>Designing Landing page</b>
+              </h2>
+              <p className="paragraph1">
+                <span>
+                  <b>2018</b>
+                </span>{" "}
+                Website
+                <br />
+              </p>
+              <p className="paragraph2">
+                Amet minim deserunt ullamco est sit aliqua dolor do amet
+                sint.Velit officia consequat duis enim velit mollit.Exercitation
+                veniam consequat sunt sunta .
+              </p>
+            </div>
+          </section>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default Works
+export default Works;
